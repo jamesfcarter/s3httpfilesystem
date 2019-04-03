1f669e02ae1d2cf484b31545c2d660edc6cd142f
James F. Carter <james@jfc.org.uk>
2019-04-03 17:02:56 +0100

    test Stat()

--- file_test.go
+++ file_test.go
@@ -10,12 +10,17 @@ import (
 	"github.com/jamesfcarter/s3httpfilesystem/mock/mock_s3iface"
 )
 
-func TestReaddir(t *testing.T) {
+func setupMock(t *testing.T) (*s3httpfilesystem.Filesystem, *mock_s3iface.MockS3API, func()) {
 	ctrl := gomock.NewController(t)
-	defer ctrl.Finish()
-
 	mock := mock_s3iface.NewMockS3API(ctrl)
 	s3f := s3httpfilesystem.NewWithS3("test", mock)
+	return s3f, mock, ctrl.Finish
+}
+
+func TestReaddir(t *testing.T) {
+	s3f, mock, finish := setupMock(t)
+	defer finish()
+
 	mock.EXPECT().
 		ListObjects((&s3.ListObjectsInput{}).
 			SetBucket("test").
@@ -67,4 +72,39 @@ func TestReaddir(t *testing.T) {
 		})
 	}
 
+}
+
+func TestStat(t *testing.T) {
+	s3f, mock, finish := setupMock(t)
+	defer finish()
+
+	mock.EXPECT().
+		ListObjects((&s3.ListObjectsInput{}).
+			SetBucket("test").
+			SetPrefix("testdir/testfile.tst").
+			SetDelimiter("/").
+			SetMaxKeys(1)).
+		Return((&s3.ListObjectsOutput{}).
+			SetContents([]*s3.Object{
+				(&s3.Object{}).
+					SetKey("testdir/testfile.tst").
+					SetSize(123),
+			}), nil)
+
+	f, err := s3f.Open("/testdir/testfile.tst")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	fi, err := f.Stat()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if fi.Name() != "testfile.tst" {
+		t.Errorf("unexpected name %s\n", fi.Name())
+	}
+	if fi.Size() != 123 {
+		t.Errorf("unexpected size %d\n", fi.Size())
+	}
 }
